28614b0d3af205c209ec9ed646b416c1e22d5c66
Brian Russell <brussell@vyatta.att-mail.com>
2020-12-07 11:36:42 +0000

    Add VCI code for filter group policer action
    Add VCI code to configure QoS filter groups using the generic
    packet classifier to apply a policer action.
    
    VRVDR-46431

--- vyatta_policy_filter_vci/filter_action.py
+++ vyatta_policy_filter_vci/filter_action.py
@@ -10,6 +10,7 @@ A module to define the filter action for QoS.
 """
 import logging
 from vyatta.proto import GPCConfig_pb2
+from vyatta_policy_qos_vci.policer import parse_bandwidth
 
 LOG = logging.getLogger('POLFIL VCI')
 
@@ -22,6 +23,11 @@ class FilterAction:
     def __init__(self, fa_dict):
         self._designation = None
         self._drop_prec = None
+        self._bandwidth = None
+        self._burst = None
+        self._excess_bandwidth = None
+        self._excess_burst = None
+        self._colour_aware = None
         self._pass = False
 
         if fa_dict is None:
@@ -32,6 +38,15 @@ class FilterAction:
         if mark_dict is not None:
             self._designation = mark_dict.get('designation')
             self._drop_prec = mark_dict.get('drop-precedence')
+
+        police_dict = fa_dict.get('police')
+        if police_dict is not None:
+            self._bandwidth = parse_bandwidth(police_dict.get('bandwidth'))
+            self._burst = police_dict.get('burst')
+            self._excess_bandwidth = parse_bandwidth(
+                police_dict.get('excess-bandwidth'))
+            self._excess_burst = police_dict.get('excess-burst')
+            self._colour_aware = police_dict.get('colour-awareness')
 
     def add_action(self, rule_message):
         """ Build protobuf actions for a rule """
@@ -52,3 +67,24 @@ class FilterAction:
             }
             action_message = rule_message.actions.add()
             action_message.colour = colour_dict.get(self._drop_prec)
+
+        # If burst values are not set, default them to 10% of
+        # the relevant configured bandwidth, ie. 100ms of burst.
+        if self._bandwidth is not None:
+            if self._burst is None:
+                self._burst = int(self._bandwidth/10)
+            if self._excess_bandwidth is None:
+                self._excess_bandwidth = 0
+                self._excess_burst = 0
+            elif self._excess_burst is None:
+                self._excess_burst = int(self._excess_bandwidth/10)
+
+            action_message = rule_message.actions.add()
+            action_message.policer.bw = self._bandwidth
+            action_message.policer.excess_bw = self._excess_bandwidth
+            action_message.policer.burst = self._burst
+            action_message.policer.excess_burst = self._excess_burst
+            if self._colour_aware == "colour-aware":
+                action_message.policer.awareness = GPCConfig_pb2.COLOUR_AWARE
+            else:
+                action_message.policer.awareness = GPCConfig_pb2.COLOUR_UNAWARE
