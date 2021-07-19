42de0c8280c3a2cb2a13e739a9177ea3c27a10ff
Aidan Gallagher <aidan.gallagher@intl.att.com>
2021-07-19 15:56:57 +0100

    Add get_all_files()
    Add description of tasks.py at header
    get_changed_files() returns fully qualified path names.
    
    VRVDR-55724

--- tasks.py
+++ tasks.py
@@ -2,6 +2,22 @@
 # Copyright (c) 2021, AT&T Intellectual Property. All rights reserved.
 # SPDX-License-Identifier: GPL-2.0-only
 
+# *************************************************
+# Developer---->|
+#               |---->tasks.py
+# Jenkins ----->|
+#
+# Jenkinsfile should be a simple & thin wrapper that calls functionality in tasks.py
+# All functions with a @task decorator are individual stages that perform some check.
+# 
+# Tasks are accesible from the command line by typing `invoke {TASKNAME}`
+# Type `invoke -l` to see a list of available commands.
+#
+# Either all files can be checked or only changed files.  
+# By default tasks.py will compare the branch you are on with master to see what has changed.
+# If you specify `all` (eg `invoke flake8 all`) then all files will be checked.
+# **************************************************
+
 import sys
 import re
 import subprocess
@@ -10,18 +26,18 @@ from typing import List
 import magic
 from invoke import task
 from invoke import Collection
+import os
 
 # ***************************************************
 # Helper functions used by multiple stages
 # ***************************************************
-
-
 def get_files_by_types(files: List, types: List[str]) -> List:
     """Return a subset of 'files' based of the file type. Use python-magic rather simply
         looking at the file extension because some of the scripts do not have any file extension"""
 
     files_by_types = []
     for file in files:
+        print(file)
         file_type = magic.from_file(file)
         if any(specified_type in file_type for specified_type in types):
             files_by_types.append(file)
@@ -34,24 +50,47 @@ def get_files_by_types(files: List, types: List[str]) -> List:
 
     return files_by_types
 
+def get_files(commits: str) -> List:
+    def get_all_files(repo_root: str) -> List:
+        """Return every file in this repository. Ignore .git folder and files excluded by .gitignore"""
+        # Most tools can search for files themselves and do not need to be passed a list of files.
+        # For example `flake8 .` will find every .py file recursively. This repo contains scripts without
+        # any file extension. Therefore it is necessary to pass every file to these tools so no files are 
+        # left out.
 
-def get_all_files() -> List:
-    """Return every file in this repository"""
+        all_files = []
+        for root, dirs, files in os.walk(repo_root):
+            if ".git" in dirs:                          # Ignore any files in .git/
+                dirs.remove(".git")
+            for file in files:
+                    all_files.append(os.path.join(root, file))
 
-    all_files = []
-    # TODO: get all files
-    return all_files
+        # Ignore any files in .gitignore
+        all_files_str = " ".join(all_files)
+        ignored_files = subprocess.check_output([f"git check-ignore {all_files_str}"], shell=True).decode("utf-8").rstrip().split()
+        valid_files = list(set(all_files) - set(ignored_files))
+        return valid_files
 
 
-def get_changed_files(commits: str) -> List:
-    """Return all the files that have changed"""
+    def get_changed_files(repo_root: str, commits: str) -> List:
+        """Return all the files that have changed"""
 
-    git_command = f"git diff --diff-filter=rd --find-renames=100% --name-only --format=format:'' {commits}"
-    result = subprocess.check_output(git_command, shell=True).decode("utf-8")
-    changed_files = result.splitlines()
+        git_command = f"git diff --diff-filter=rd --find-renames=100% --name-only --format=format:'' {commits}"
+        result = subprocess.check_output(git_command, shell=True).decode("utf-8")
+        changed_files = result.splitlines()
 
-    print(f"Files to check {changed_files}\n")
-    return changed_files
+        print(f"Files to check {changed_files}\n")
+        changed_files_full_path = [repo_root + '/' + s for s in changed_files]
+        return changed_files_full_path
+
+    # Get the root of the git repo. e.g /home/ag474u/Code/vplane-config-qos. 
+    repo_root =  subprocess.check_output(['git','rev-parse','--show-toplevel']).decode("utf-8").rstrip()
+
+    if commits == "all":
+        return get_all_files(repo_root)
+    else:
+        return get_changed_files(repo_root, commits)
+
 
 # ***************************************************
 # Stages of the pipeline
@@ -89,7 +128,7 @@ def licence(context, commits="master...HEAD"):
                     error = True
         return error
 
-    files = get_changed_files(commits)
+    files = get_files(commits)
     code_files = get_files_by_types(files, ["Python", "Perl", "Bourne-Again shell", "Yang"])
     att_error = check_att_licence(code_files)
     spdx_error = check_spdx_licence(code_files)
@@ -138,7 +177,7 @@ def yang(context, commits="master...HEAD"):
 
         return context.run(dram_command, echo=True)
 
-    files = get_changed_files(commits)
+    files = get_files(commits)
     yang_files = get_files_by_types(files, ["Yang"])
     # If there are no yang changes then can skip this
     if yang_files:
@@ -152,7 +191,7 @@ def yang(context, commits="master...HEAD"):
 @task
 def flake8(context, commits="master...HEAD"):
     """Run flake8 over changed files"""
-    files = get_changed_files(commits)
+    files = get_files(commits)
     python_files = get_files_by_types(files, ["Python"])
     if python_files:  # Only run flake8 if there are files to check (otherwise it will run it over the directory)
         python_files = " ".join(python_files)
@@ -161,7 +200,7 @@ def flake8(context, commits="master...HEAD"):
 @task
 def mypy(context, commits="master...HEAD"):
     """Run python static type checker"""
-    files = get_changed_files(commits)
+    files = get_files(commits)
     python_files = get_files_by_types(files, ["Python"])
     if python_files:  # Only run mypy if there are files to check (otherwise it will run it over the directory)
         python_files = " ".join(python_files)
@@ -192,5 +231,6 @@ def gitlint(context, commits="master...HEAD"):
 
 @task(pre=[licence, pytest, flake8, gitlint, yang])
 def all(context, commits="master...HEAD"):
-    """Run all stages in the pipeline. Use invoke pre tasks to invoke all stages"""
+    """Run all stages in the pipeline.
+       Use invoke pre tasks to invoke all stages"""
     pass
